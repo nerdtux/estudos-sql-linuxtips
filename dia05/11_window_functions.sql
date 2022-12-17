7810ea5e52f7d835aab960a0a917ae3cea70c996
nerdtux <nerdtux>
2022-12-17 01:00:35 +0000

    Descomplicando SQL

---
+++ dia05/11_window_functions.sql
@@ -0,0 +1,128 @@
+-- Databricks notebook source
+WITH tb_vendas_vendedores AS (
+
+  SELECT idVendedor,
+         COUNT(*) AS qtVendas
+  FROM silver_olist.item_pedido
+  GROUP BY idVendedor
+  ORDER BY qtVendas DESC
+)
+
+SELECT * FROM tb_vendas_vendedores
+
+-- COMMAND ----------
+
+WITH tb_vendas_vendedores AS (
+
+  SELECT idVendedor,
+         COUNT(*) AS qtVendas
+  FROM silver_olist.item_pedido
+  GROUP BY idVendedor
+  ORDER BY qtVendas DESC
+)
+
+SELECT T1.*,
+       T2.descUF
+
+FROM tb_vendas_vendedores AS T1
+
+LEFT JOIN silver_olist.vendedor AS T2
+ON T1.idVendedor = T2.idVendedor
+
+ORDER BY qtVendas DESC
+
+LIMIT 10
+
+-- COMMAND ----------
+
+WITH tb_vendas_vendedores AS (
+
+  SELECT idVendedor,
+         COUNT(*) AS qtVendas
+  FROM silver_olist.item_pedido
+  GROUP BY idVendedor
+  ORDER BY qtVendas DESC
+)
+
+SELECT T1.*,
+       T2.descUF,
+       ROW_NUMBER() OVER (PARTITION BY T2.descUF ORDER BY qtVendas DESC) AS RN
+
+FROM tb_vendas_vendedores AS T1
+
+LEFT JOIN silver_olist.vendedor AS T2
+ON T1.idVendedor = T2.idVendedor
+
+ORDER BY descUF, qtVendas DESC
+
+
+
+-- COMMAND ----------
+
+WITH tb_vendas_vendedores AS (
+
+  SELECT idVendedor,
+         COUNT(*) AS qtVendas
+  FROM silver_olist.item_pedido
+  GROUP BY idVendedor
+  ORDER BY qtVendas DESC
+),
+
+tb_row_number AS (
+
+  SELECT T1.*,
+         T2.descUF,
+         ROW_NUMBER() OVER (PARTITION BY T2.descUF ORDER BY qtVendas DESC) AS RN
+
+  FROM tb_vendas_vendedores AS T1
+
+  LEFT JOIN silver_olist.vendedor AS T2
+  ON T1.idVendedor = T2.idVendedor
+
+  ORDER BY descUF, qtVendas DESC
+
+)
+
+SELECT * FROM tb_row_number WHERE RN <= 10
+
+-- COMMAND ----------
+
+WITH tb_vendas_vendedores AS (
+
+  SELECT idVendedor,
+         COUNT(*) AS qtVendas
+  FROM silver_olist.item_pedido
+  GROUP BY idVendedor
+  ORDER BY qtVendas DESC
+),
+
+tb_row_number AS (
+
+  SELECT T1.*,
+         T2.descUF,
+         ROW_NUMBER() OVER (PARTITION BY T2.descUF ORDER BY qtVendas DESC) AS RN
+
+  FROM tb_vendas_vendedores AS T1
+
+  LEFT JOIN silver_olist.vendedor AS T2
+  ON T1.idVendedor = T2.idVendedor
+
+  QUALIFY RN <= 10
+  
+  ORDER BY descUF, qtVendas DESC
+
+)
+
+SELECT * FROM tb_row_number
+
+-- COMMAND ----------
+
+SELECT *
+
+FROM A
+
+WHERE -- filtra na fonte
+
+QUALIFY -- filtra Window Funciotn
+
+HAVING -- filtra GROUP BY
